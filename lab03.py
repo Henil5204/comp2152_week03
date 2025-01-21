795b340332f366d6517f42cee03f99ad1f3c1633
Henil5204 <101511850@georgebrown.ca>
2025-01-21 11:16:17 -0500

    comment added

--- lab03.py
+++ lab03.py
@@ -1,6 +1,6 @@
 import random
 
-# Setup
+# Dice options using list() and range 
 diceOptions = list(range(1, 7))
 weapons = ['Fist', 'Knife', 'Club', 'Gun', 'Bomb', 'Nuclear Bomb']
 print("Available Weapons:", ', '.join(weapons))
